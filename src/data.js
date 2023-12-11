4a75f2ec73a1fa752e7ccd175abb4cf79e46673b
JonatanDrysen <jojodrysen@gmail.com>
2023-12-11 13:15:47 +0100

    add data

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const data = [
+    {
+        img: "https://upload.wikimedia.org/wikipedia/en/4/4f/DIIV_-_Is_the_Is_Are.jpg",
+        album: "Is the Is Are",
+        band: "DIIV"
+    },
+    {
+        img: "https://upload.wikimedia.org/wikipedia/en/7/7b/DIIV_-_Oshin.png",
+        album: "Oshin",
+        band: "DIIV"
+    },
+    {
+        img: "https://upload.wikimedia.org/wikipedia/en/e/ed/The-Queen-is-Dead-cover.png",
+        album: "The Queen Is Dead",
+        band: "The Smiths"
+    },
+    {
+        img: "https://upload.wikimedia.org/wikipedia/en/d/d9/Just_For_a_Day.jpg",
+        album: "Just For A Day",
+        band: "Slowdive"
+    },
+    {
+        img: "https://upload.wikimedia.org/wikipedia/en/8/89/Beach_Fossils_2010.jpg",
+        album: "Beach Fossils",
+        band: "Beach Fossils"
+    },
+    {
+        img: "https://upload.wikimedia.org/wikipedia/en/2/22/Superheaven_-_Jar%2C_Cover_%28Current_Pressing%29.jpg",
+        album: "Jar",
+        band: "Superheaven"
+    },
+]
