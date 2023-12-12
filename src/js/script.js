7d61314d060b24ca7afb29f699ba9be03670a1fd
JonatanDrysen <jojodrysen@gmail.com>
2023-12-12 11:51:22 +0100

    wrap js inside function

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,28 @@
 import { data } from "./data.js"
 
-console.log(data)
-let list = document.getElementById("songList")
-
-data.forEach((item) => {
-    let img = document.createElement("img")
-    let song = document.createElement("p")
-    let band = document.createElement("p")
-    let li = document.createElement("li")
+function renderList() {
+    let list = document.getElementById("songList")
     
-    img.setAttribute("src", item.img)
-    song.innerText = item.song
-    band.innerText = item.band
-
-    li.appendChild(song)
-    li.appendChild(band)
-    li.appendChild(img)
-
-    const documentFragment = document.createDocumentFragment()
-    documentFragment.appendChild(li)
+    data.forEach((item) => {
+        let img = document.createElement("img")
+        let song = document.createElement("p")
+        let band = document.createElement("p")
+        let li = document.createElement("li")
+        
+        img.setAttribute("src", item.img)
+        song.innerText = item.song
+        band.innerText = item.band
     
-    list.appendChild(documentFragment)
-})
+        li.appendChild(song)
+        li.appendChild(band)
+        li.appendChild(img)
+    
+        let documentFragment = document.createDocumentFragment()
+        documentFragment.appendChild(li)
+    
+        list.appendChild(documentFragment)
+    })
+}
+
+renderList()
+
