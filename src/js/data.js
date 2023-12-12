04fa1b4b1804dcc10d9701282072575615c242e6
JonatanDrysen <jojodrysen@gmail.com>
2023-12-12 11:42:51 +0100

    add js script to render data from local file

--- src/js/data.js
+++ src/js/data.js
@@ -1,32 +1,38 @@
-const data = [
+export const data = [
     {
         img: "https://upload.wikimedia.org/wikipedia/en/4/4f/DIIV_-_Is_the_Is_Are.jpg",
         album: "Is the Is Are",
+        song: "Dust",
         band: "DIIV"
     },
     {
         img: "https://upload.wikimedia.org/wikipedia/en/7/7b/DIIV_-_Oshin.png",
         album: "Oshin",
+        song: "Doused",
         band: "DIIV"
     },
     {
         img: "https://upload.wikimedia.org/wikipedia/en/e/ed/The-Queen-is-Dead-cover.png",
         album: "The Queen Is Dead",
+        song: "Some Girls Are Bigger Than Others",
         band: "The Smiths"
     },
     {
         img: "https://upload.wikimedia.org/wikipedia/en/d/d9/Just_For_a_Day.jpg",
         album: "Just For A Day",
+        song: "Slowdive",
         band: "Slowdive"
     },
     {
         img: "https://upload.wikimedia.org/wikipedia/en/8/89/Beach_Fossils_2010.jpg",
         album: "Beach Fossils",
+        song: "Vacation",
         band: "Beach Fossils"
     },
     {
         img: "https://upload.wikimedia.org/wikipedia/en/2/22/Superheaven_-_Jar%2C_Cover_%28Current_Pressing%29.jpg",
         album: "Jar",
+        song: "Youngest Daughter",
         band: "Superheaven"
     },
 ]
